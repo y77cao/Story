50805654c0e66333d2ddfbfd82711031761966d2
y77cao <yujiac8000@gmail.com>
2022-09-11 23:12:25 -0400

    tokenUri

--- contracts/Story.sol
+++ contracts/Story.sol
@@ -11,17 +11,18 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
 
 import "./StringUtils.sol";
 
 contract Story is ERC721, IERC2981, Ownable, Pausable, ReentrancyGuard {
     using Strings for uint256;
 
-    string private baseURI;
     address private openSeaProxyRegistryAddress;
     bool private isOpenSeaProxyActive = true;
 
-    uint256 public constant pricePerChar = 0.0003 ether;
+    uint256 public constant pricePerChar = 0.0005 ether;
     uint256 public constant maxCharsPerMint = 280;
     uint256 public constant maxCharsPerTitle = 32;
     uint256 public constant numberOfMintRequiredToStartStory = 10;
@@ -63,12 +64,13 @@ contract Story is ERC721, IERC2981, Ownable, Pausable, ReentrancyGuard {
         returns (uint256)
     {
         uint256 textCharCount = StringUtils.strlen(text);
+        TokenMetadata memory parentMetadata = mintedTokens[parentId];
         require(textCharCount <= maxCharsPerMint, 'Number of characters exceeds limit');
         require(
             pricePerChar * textCharCount == msg.value,
             "Incorrect ETH value sent"
         );
-
+        require(parentMetadata.isBeginning, "Text must belong to a story");
         if (tx.origin != msg.sender) revert();
 
         uint256 nextId = mintedTokens.length;
@@ -119,7 +121,7 @@ contract Story is ERC721, IERC2981, Ownable, Pausable, ReentrancyGuard {
     }
 
     function withdraw(uint256 tokenId, uint256 amount) public {
-      uint256 balance = balanceOf(tokenId);
+      uint256 balance = getBalanceOf(tokenId);
       require(balance >= amount, "Insufficient balance");
       require(ownerOf(tokenId) == msg.sender, "Unauthorized attempt to withdraw");
 
@@ -127,14 +129,11 @@ contract Story is ERC721, IERC2981, Ownable, Pausable, ReentrancyGuard {
       metadata.withdrawn += amount;
 
       mintedTokens[tokenId] = metadata;
-      payable(msg.sender).transfer(amount);
+      (bool sent,) = (msg.sender).call{value: amount}("");
+      require(sent, "Failed to withdraw");
     }
 
     // ============ PUBLIC READ-ONLY FUNCTIONS ============
-
-    function getBaseURI() external view returns (string memory) {
-        return baseURI;
-    }
 
     function getLastTokenId() external view returns (uint256) {
         return mintedTokens.length - 1;
@@ -144,7 +143,7 @@ contract Story is ERC721, IERC2981, Ownable, Pausable, ReentrancyGuard {
         return SafeMath.mul(numberOfChars, pricePerChar);
     }
 
-    function balanceOf(uint256 tokenId) public view tokenExists(tokenId) returns (uint256) {
+    function getBalanceOf(uint256 tokenId) public view tokenExists(tokenId) returns (uint256) {
       uint256 balance = 0;
       TokenMetadata memory metadata = mintedTokens[tokenId];
       for (uint256 i = tokenId; i < mintedTokens.length; i++) {
@@ -164,10 +163,6 @@ contract Story is ERC721, IERC2981, Ownable, Pausable, ReentrancyGuard {
       return balance;
     }
 
-    function getTotalMintedCount() public view returns (uint256) {
-      return mintedTokens.length;
-    }
-
     function creatorAt(uint256 tokenId) public view tokenExists(tokenId) returns (address) {
       return mintedTokens[tokenId].creator;
     }
@@ -181,10 +176,6 @@ contract Story is ERC721, IERC2981, Ownable, Pausable, ReentrancyGuard {
     }
 
     // ============ OWNER-ONLY ADMIN FUNCTIONS ============
-
-    function setBaseURI(string memory _baseURI) external onlyOwner {
-        baseURI = _baseURI;
-    }
 
     function setIsOpenSeaProxyActive(bool _isOpenSeaProxyActive)
         external
@@ -245,24 +236,76 @@ contract Story is ERC721, IERC2981, Ownable, Pausable, ReentrancyGuard {
         return super.isApprovedForAll(owner, operator);
     }
 
-    /**
-     * @dev See {IERC721Metadata-tokenURI}.
-     */
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        tokenExists(tokenId)
-        returns (string memory)
-    {
-        return
-            string(abi.encodePacked(baseURI, "/", tokenId.toString(), ".json"));
-    }
+    function tokenURI(uint256 tokenId) public view override tokenExists(tokenId) returns (string memory) {
+    bytes memory dataURI = abi.encodePacked(
+        '{',
+            '"name": "Story #', tokenId.toString(), '",',
+            '"image": "', generateSvg(tokenId), '"',
+            '"attributes: "', getTokenAttributes(tokenId),'"',
+        '}'
+    );
+    return string(
+        abi.encodePacked(
+            "data:application/json;base64,",
+            Base64.encode(dataURI)
+        )
+    );
+}
 
-    /**
-     * @dev See {IERC165-royaltyInfo}.
-     */
+   function generateSvg(uint256 tokenId) internal view tokenExists(tokenId) returns (string memory) {
+       TokenMetadata memory metadata = mintedTokens[tokenId];
+       bytes memory svg = abi.encodePacked(
+        '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+        '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
+        '<rect width="100%" height="100%" fill="black" />',
+        '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">', string(metadata.text), '</text>',
+        '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "--", getAuthor(tokenId, metadata.owner),'</text>',
+        // TODO title, date
+        '</svg>'
+    );
+    return string(
+        abi.encodePacked(
+            "data:image/svg+xml;base64,",
+            Base64.encode(svg)
+        )    
+    );
+   }
+
+   function getTokenAttributes(uint256 tokenId) internal view tokenExists(tokenId) returns (string memory) {
+       TokenMetadata memory metadata = mintedTokens[tokenId];
+       return abi.encodePacked(
+        '[{"trait_type":"Author", "value": ',
+        getAuthor(tokenId, metadata.owner),
+        '}, {"trait_type":"Word Count", "value": "',
+        StringUtils.strlen(metadata.text).toString(),
+        '"}]'
+        // TODO title, date
+      );
+   }
+
+   function getAuthor(
+    uint256 tokenId,
+    address ownerAddress
+  ) internal view tokenExists(tokenId) returns (bytes memory) {
+    string memory authorEns = ENSNameResolver.getENSName(ownerAddress);
+    return
+        bytes(authorEns).length > 0
+          ? authorEns
+          : string(abi.encodePacked('0x', toAsciiString(ownerAddress)));
+  }
+
+   function toAsciiString(address x) internal pure returns (string memory) {
+    bytes memory s = new bytes(40);
+    for (uint256 i; i < 20; i++) {
+      bytes1 b = bytes1(uint8(uint256(uint160(x)) / (2**(8 * (19 - i)))));
+      bytes1 hi = bytes1(uint8(b) / 16);
+      bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+      s[2 * i] = char(hi);
+      s[2 * i + 1] = char(lo);
+    }
+    return string(s);
+  }
+
     function royaltyInfo(uint256 tokenId, uint256 salePrice)
         external
         view
